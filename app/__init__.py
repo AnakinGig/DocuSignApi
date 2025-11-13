eba7fac5fb3c79bb4e1339b4493524546002bfee
AnakinGig <gigantanakin19@gmail.com>
2025-11-13 13:50:40 +0100

    Change internal api base url

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from .routes import api_bp
+import os
 
 def create_app():
   app = Flask(__name__)
+
+  app.config["INTERNAL_API_BASE_URL"] = os.environ.get("INTERNAL_API_BASE_URL", "http://localhost:5001")
 
   app.register_blueprint(api_bp, url_prefix="/api")
   
