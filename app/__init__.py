f017d12138026fa088148932a5fd499bb897766f
AnakinGig <gigantanakin19@gmail.com>
2025-11-24 12:00:47 +0100

    Added max content length for pdf

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,8 @@ def create_app():
   )
 
   app.config["INTERNAL_API_BASE_URL"] = os.environ.get("INTERNAL_API_BASE_URL", "http://localhost:5001")
-
+  app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10 MB
+  
   app.register_blueprint(docusign_bp, url_prefix="/api")
   app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
   
