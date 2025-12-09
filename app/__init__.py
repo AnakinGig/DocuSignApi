edcff15b7ccb3bba13864d77cfe41987c71450c3
AnakinGig <gigantanakin19@gmail.com>
2025-12-09 12:54:38 +0100

    Test to add flasgger

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,17 @@
 from flask import Flask
 from flask_swagger_ui import get_swaggerui_blueprint
 from .docusign_api import docusign_bp
+from .swagger_setup import init_swagger
 import os
 
 def create_app():
   app = Flask(__name__)
-  
-  SWAGGER_URL = '/docs'
-  API_URL = '/static/swagger.yaml'
-  
-  swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={'app_name': "DocuSign API Service"}
-  )
 
   app.config["INTERNAL_API_BASE_URL"] = os.environ.get("INTERNAL_API_BASE_URL", "http://localhost:5001")
   app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024  # 10 MB
   
   app.register_blueprint(docusign_bp, url_prefix="/api")
-  app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+  init_swagger(app)
   
   @app.route('/')
   def helloWord():
